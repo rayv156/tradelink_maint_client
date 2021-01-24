81b870e8ddab0d65e1a6893887f0a6837914a1ff
Ray Velasquez <rayv156@gmail.com>
2021-01-24 17:36:18 -0600

    added create request page, request is working but repairs are not posting correctly

--- src/components/RequestCreate.js
+++ src/components/RequestCreate.js
@@ -7,26 +7,58 @@ const RequestCreate = () => {
     const {gState, setgState} = React.useContext(GlobalCtx)
     const {url} = gState
     const blank = {
-        date: "",
+        date: Date(),
         equipment: "",
         odometer: 0,
-        status: 0,
-        user_id: gState.user_id
+        status: ""
     }
     const [formRequest, setFormRequest] = React.useState(blank)
+    const [inputList, setInputList] = React.useState([{ repair_type: "", description: "", status: "", request_id: gState.request_id }]);
 
     const handleChange = (event) => {
         setFormRequest({...formRequest, [event.target.name]: event.target.value})
     }
 
-    const handleSubmit = (event) => {
+    const handleInputChange = (e, index) => {
+        const { name, value } = e.target;
+        const list = [...inputList];
+        list[index][name] = value;
+        setInputList(list);
+      };
+
+      const handleRemoveClick = index => {
+        const list = [...inputList];
+        list.splice(index, 1);
+        setInputList(list);
+      };
+     
+      // handle click event of the Add button
+      const handleAddClick = () => {
+        setInputList([...inputList, { repair_type: "", description: "", status: "", request_id: gState.request_id }]);
+      };
+
+    const handleSubmit = async (event) => {
         event.preventDefault()
-        fetch(`${url}/requests`, {
+        const token = await window.localStorage.getItem("token")
+        await fetch(`${url}/requests`, {
             method: "post",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "authorization": `bearer ${token}`
             },
             body: JSON.stringify(formRequest)
+        })
+        .then(response => response.json())
+        .then((data) => {
+            setgState({...gState, request_id: data.id})
+        })
+        await fetch(`${url}/repairs`, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": `bearer ${token}`
+            },
+            body: JSON.stringify(inputList)
         })
         .then(response => response.json())
         .then(<Redirect to="/requests"/>)
@@ -44,7 +76,7 @@ return (
                 type="date"
                 name='date'
                 className='form-control'
-                placeholder={Date()}
+                
                 value={formRequest.date}
                 onChange={handleChange} />
             
@@ -80,12 +112,60 @@ return (
                 value={formRequest.status}
                 onChange={handleChange} />
         </div>
-        
+
+        <div className="App">
+      
+      {inputList.map((x, i) => {
+        return (<>
+          <div className="form-group form-inline">
+              <label>Repair Type:</label>
+            <input
+              className="form-control"
+              name="repair_type"
+              placeholder="Repair Type"
+              value={x.repair_type}
+              onChange={e => handleInputChange(e, i)}
+            />
+            </div>
+            <div className="form-group form-inline">
+            <label>Description</label>
+            <input
+              className="form-control"
+              name="description"
+              placeholder="Description"
+              value={x.description}
+              onChange={e => handleInputChange(e, i)}
+            />
+            </div>
+            <div className="form-group form-inline">
+            <label>Status</label>
+            <input
+              className="form-control"
+              name="status"
+              placeholder="Status"
+              value={x.status}
+              onChange={e => handleInputChange(e, i)}
+            />
+            </div>
+            <div className="btn-box">
+              {inputList.length !== 1 && <button
+                className="btn btn-danger"
+                onClick={() => handleRemoveClick(i)}>Remove</button>}
+              {inputList.length - 1 === i && <button className="btn btn-success" onClick={handleAddClick}>Add</button>}
+            </div>
+          </>
+        );
+      })}
+    </div>
         
         <p style={{color: 'red'}}>{gState.error}</p>
+        <br>
+
+
+        </br>
         <button
             className="btn btn-primary btn-block"
-            type="submit">Submit</button>
+            type="submit">Submit Request</button>
     </form>
 </div>
     )
